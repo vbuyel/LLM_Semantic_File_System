6031e80f6935d0100fceb0ebf5c215c8e11b8ce9
vbuyel <vladosbuyel@gmail.com>
2026-04-17 19:09:33 +0300

    first cleanup rag_search

--- src/llm/adapters/rag_search.py
+++ src/llm/adapters/rag_search.py
@@ -1,9 +1,7 @@
-from ast import Pass
 import os
-from langchain_text_splitters import CharacterTextSplitter
-from openai import OpenAI
 from typing import List
 from sentence_transformers import SentenceTransformer
+import requests
 
 from src.llm.domain.domain import (
     RAGRequest,
@@ -13,63 +11,18 @@ from src.llm.domain.domain import (
 
 class RAGSearch:
     def __init__(self):
-        self.client = OpenAI(
-            base_url="https://openrouter.ai/api/v1",
-            api_key=os.getenv("OPENROUTER_API_KEY"),
-        )
-        self.model_qa = os.getenv("MODEL")
-
         self.model_embed = SentenceTransformer(os.getenv("MODEL_EMBED"))
-        self.num_top_results = 5
-        self.text_splitter = CharacterTextSplitter(
-            separator="\n",
-            chunk_size=250,
-            chunk_overlap=30,
-            is_separator_regex=False,
-        )
-
-    # def __get_text_from_file(self, file_path: str) -> str:
-    #     loader = UnstructuredLoader(file_path)
-    #     loaded_docs = loader.load()
-    #     return "\n\n".join(
-    #         doc.page_content
-    #         for doc in loaded_docs
-    #         if getattr(doc, "page_content", None)
-    #     )
-
-    # def _extract_text_from_file(self, file_path: str) -> List[str]:
-    #     text_from_file = self.__get_text_from_file(file_path)
-    #     splited_texts = self.text_splitter.split_text(text_from_file)
-    #     return splited_texts
-
-    # def _get_file_content_based_on_query_text(
-    #     self, query: DataForExtraction
-    # ) -> DataExtracted:
-    #     doc_embeddings = self.model_embed.encode(query.additional_data).astype("float32")
-
-    #     index = faiss.IndexFlatL2(doc_embeddings.shape[1])
-    #     index.add(doc_embeddings)
-
-    #     query_embedding = self.model_embed.encode([query.text]).astype("float32")
-    #     _, indexes = index.search(query_embedding, self.num_top_results)
-
-    #     founded_context = ""
-    #     for ind in indexes[0]:
-    #         if ind < len(query.additional_data):
-    #             founded_context += query.additional_data[ind] + "\n"
-
-    #     return DataExtracted(text=founded_context)
 
 
-    def _encode_user_query(query: str) -> List[float]:
-        pass
+    def _encode_user_query(self, query: str) -> List[float]:
+        return self.model_embed.encode([query]).astype("float32")
 
 
-    def _get_most_relevant_text_from_files() -> List[str]:
-        pass
+    def _get_most_relevant_text_from_files(self, encoded_query: List[float]) -> List[str]:
+        return requests.get("http://localhost:9092/send/vector_db", params={"data": encoded_query})
 
 
     def do_search(self, query: RAGRequest) -> RAGResponse:
         encoded_query = self._encode_user_query(query.text)
-        best_text_parts = self._get_most_relevant_text_from_files(encoded_query) # connect to vector db
+        best_text_parts = self._get_most_relevant_text_from_files(encoded_query) # connect to kafka -> vector db
         return RAGResponse(text=best_text_parts)
